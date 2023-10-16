c1129e35411a2ab50b198a6478fb032433947279
handrawanw <handrawanw@gmail.com>
2023-10-17 01:20:24 +0700

    setup database

--- database/migrations/20231017_2_payment.js
+++ database/migrations/20231017_2_payment.js
@@ -9,7 +9,7 @@ exports.up = function(knex) {
       table.integer("id_product").references("id").inTable("product").notNullable();
       table.bigInteger("created_by").references("id").inTable("users").defaultTo(null);
       table.bigInteger("created_at").defaultTo(Date.now());
-      table.bigInteger("expired_at").defaultTo(Date.now());
+      table.bigInteger("expired_at").defaultTo(Date.now()+1200000);// 20 Menit
       table.smallint("is_deleted").defaultTo(0);
     });
   };
